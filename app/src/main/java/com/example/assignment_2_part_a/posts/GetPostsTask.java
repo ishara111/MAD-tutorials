7d503b4776a328c1a6455051f01c52f66fa010d2
ishara111 <isharagomes111@gmail.com>
2022-10-08 21:14:23 +0530

    getpoststask class added

---
+++ app/src/main/java/com/example/assignment_2_part_a/posts/GetPostsTask.java
@@ -0,0 +1,10 @@
+package com.example.assignment_2_part_a.posts;
+
+import java.util.concurrent.Callable;
+
+public class GetPostsTask implements Callable<String> {
+    @Override
+    public String call() throws Exception {
+        return null;
+    }
+}
