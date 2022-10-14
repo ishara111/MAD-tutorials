4f59a1b5e73ae3972e08eb3fce106632a983b3b7
ishara111 <isharagomes111@gmail.com>
2022-10-14 15:26:56 +0530

    fragments added

---
+++ app/src/main/java/com/example/assignment_2_part_b/multiple_images/MultipleImageAdapter.java
@@ -0,0 +1,4 @@
+package com.example.assignment_2_part_b.multiple_images;
+
+public class MultipleImageAdapter {
+}
---
+++ app/src/main/java/com/example/assignment_2_part_b/single_image/SingleImageAdapter.java
@@ -0,0 +1,4 @@
+package com.example.assignment_2_part_b.single_image;
+
+public class SingleImageAdapter {
+}
